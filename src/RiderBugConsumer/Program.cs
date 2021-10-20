beef8079a5ae41605def1e3e7640738fbf796e7d
George Drak <sonicgd@gmail.com>
2021-10-20 17:41:51 +0500

    feat: initial commit

---
+++ src/RiderBugConsumer/Program.cs
@@ -0,0 +1,28 @@
+﻿using RiderBugPackage;
+
+var consumerAbstractInheritedBugRecord = new ConsumerAbstractInheritedBugRecord();
+Console.WriteLine(consumerAbstractInheritedBugRecord);
+
+public record ConsumerAbstractBugRecord : AbstractBugRecord
+{
+}
+
+public record ConsumerAbstractInheritedBugRecord : AbstractInheritedBugRecord
+{
+}
+
+public record ConsumerBugRecord : BugRecord
+{
+}
+
+public record ConsumerInheritedBugRecord : InheritedBugRecord
+{
+}
+
+public record ConsumerInheritedFromAbstractBugRecord : InheritedFromAbstractBugRecord
+{
+}
+
+public record ConsumerAbstractInheritedFromNonAbstractBugRecord : AbstractInheritedFromNonAbstractBugRecord
+{
+}
---
+++ src/RiderBugPackage/TestRecord.cs
@@ -0,0 +1,25 @@
+﻿namespace RiderBugPackage;
+
+public abstract record AbstractBugRecord
+{
+}
+
+public abstract record AbstractInheritedBugRecord : AbstractBugRecord
+{
+}
+
+public record BugRecord
+{
+}
+
+public record InheritedBugRecord : BugRecord
+{
+}
+
+public record InheritedFromAbstractBugRecord : AbstractBugRecord
+{
+}
+
+public abstract record AbstractInheritedFromNonAbstractBugRecord : BugRecord
+{
+}
